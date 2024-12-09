31e9ea74e91c7074048f96bf5d8eb83969cfa404
Noviiich <nowikoff63@gmail.com>
2024-12-09 19:13:48 +0300

    move function in handler

--- handler/functions-handler.go
+++ handler/functions-handler.go
@@ -1,7 +1,38 @@
 package handler
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+var counter int
+var mutex sync.Mutex // Мьютекс для защиты доступа к counter
 
 func InitilizeHandler() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
+}
+
+func InitilizeHandleFunctions() {
+	http.HandleFunc("/", serverFile)
+
+	http.HandleFunc("/home", homeString)
+
+	http.HandleFunc("/counter", incrementCounter)
+}
+
+func serverFile(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, r.URL.Path[1:])
+}
+
+func homeString(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Дом")
+}
+
+func incrementCounter(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	counter++
+	fmt.Fprintf(w, strconv.Itoa(counter))
+	mutex.Unlock()
 }
