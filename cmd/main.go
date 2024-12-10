93527049671587fc4abe0441d274fd99a4695f8d
Noviiich <nowikoff63@gmail.com>
2024-12-10 17:11:06 +0300

    apply config settings

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 
+	"github.com/Noviiich/golang-web-server/configs"
 	"github.com/Noviiich/golang-web-server/mhttp"
 )
 
@@ -10,17 +11,11 @@ var (
 	useHandlerFunctions bool
 )
 
-const (
-	url             string = "localhost"
-	port            string = "8080"
-	staticDirectory string = "./static"
-)
-
 func main() {
 	flag.BoolVar(&useHandlerFunctions, "use-handler-functions", true, "Runs the Web Browser with handler functions")
 	flag.Parse()
 
-	server := mhttp.NewServer(url, port, staticDirectory)
+	server := mhttp.NewServer(configs.URL, configs.PORT, configs.STATIC_DIRECTORY)
 
 	if useHandlerFunctions {
 		server.InitilizeHandleFunctions()
